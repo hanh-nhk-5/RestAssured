8f63753a89434f4c7cc359902171ec62e92f6bab
-hanhnhk <hanhnhk@gmail.com>
2026-07-14 18:04:33 -0500

    first commit

---
+++ src/test/java/tests/JsonTest.java
@@ -0,0 +1,58 @@
+package tests;
+
+import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import resources.payloads.Course;
+
+import java.util.List;
+import java.util.Map;
+
+public class JsonTest {
+    @Test
+    public void testAccessJson(){
+        JsonPath jsonPath = new JsonPath(Course.CoursePricePayload());
+
+        //get size of an array
+        int numberOfCourses = jsonPath.getInt("courses.size()");
+        Assert.assertEquals(numberOfCourses,4);
+
+        //access child
+        int purchaseAmount = jsonPath.getInt("dashboard.purchaseAmount");
+        Assert.assertEquals(purchaseAmount,1162);
+
+        //access an element of an array using index
+        String title = jsonPath.getString("courses[0].title");
+        Assert.assertEquals(title,"Selenium Python");
+
+        //loop through an array to access value
+        int price;
+        for(int i=0;i<numberOfCourses;i++){
+            title = jsonPath.getString("courses["+ i +"].title");
+            price = jsonPath.getInt("courses["+ i +"].price");
+            System.out.println(title + " course - price: $" + price);
+        }
+
+        //find an element in an array and access its attribute
+        int copies = jsonPath.getInt("courses.find {it.title == 'RPA'}.copies");
+        System.out.println("The RPA course has " + copies + " copies"); ;
+
+        //find an element in an array and return a map
+        Map<String, Object> course = jsonPath.getMap("courses.find {it.title == 'Cypress'}");
+        System.out.println(course);
+
+        //find an element in an array by multiple conditions
+        List<Map<String, Object>> courses = jsonPath.getList("courses.findAll {it.title == 'Cypress' || it.price == 36}");
+        for(Map<String, Object> c: courses){
+            System.out.println("The course " + c.get("title") + " costs $" + c.get("price") + " and there is "+ c.get("copies")+" copies");
+        }
+
+        courses = jsonPath.getList("courses");
+        int sum = 0;
+        for(Map<String, Object> c: courses){
+            sum += Integer.parseInt(c.get("price").toString());
+        }
+        Assert.assertEquals(sum,purchaseAmount);
+    }
+
+}
---
+++ src/test/java/tests/MapTest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.testng.annotations.Test;
+import resources.payloads.Place;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class MapTest {
+
+    @Test
+    public void testMapAPI(){
+        //S1: add a place into the map and get place_id from the api response using JsonPath
+        RestAssured.baseURI= "https://rahulshettyacademy.com";
+        String response= given().log().all().queryParam("key", "qaclick123").body(Place.getAddPlacePayload())
+                        .when().post("/maps/api/place/add/json")
+                        .then().assertThat().statusCode(200).extract().response().body().asString();
+        JsonPath jsonPath = new JsonPath(response);
+        String place_id = jsonPath.get("place_id");
+
+        //S2: update address of the place
+        String expectedAddress= "123 somewhere, TX";
+        given().log().all().queryParam("key", "qaclick123").queryParam("place_id", place_id)
+                .body(Place.GetUpdatePlacePayload(place_id, expectedAddress))
+                .when().put("/maps/api/place/update/json")
+                .then().assertThat().statusCode(200).body("msg", equalTo("Address successfully updated"));
+
+        //S3: call getPlace API and verify whether the address is updated
+        given().log().all().queryParam("key", "qaclick123").queryParam("place_id", place_id)
+                .when().get("/maps/api/place/get/json")
+                .then().assertThat().statusCode(200).body("address", equalTo(expectedAddress));
+
+    }
+}
