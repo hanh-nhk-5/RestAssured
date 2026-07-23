a01a38f95ccc8d067625e4aade522765a111a502
-hanhnhk <hanhnhk@gmail.com>
2026-07-23 10:44:58 -0500

    refactor CourseOAuthTest.java

--- src/test/java/tests/CourseOAuthTest.java
+++ src/test/java/tests/CourseOAuthTest.java
@@ -1,40 +1,67 @@
 package tests;
 
-import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.testng.annotations.Test;
+import static org.testng.Assert.*;
 import resources.pojo.course.GetCourseDetailsResponse;
 
 import static io.restassured.RestAssured.given;
 
 public class CourseOAuthTest {
+    private static final String BASE_URI = "https://rahulshettyacademy.com";
+    private static final String AUTHORIZATION_ENDPOINT = "/oauthapi/oauth2/resourceOwner/token";
+    private static final String GET_COURSE_DETAILS_ENDPOINT = "/oauthapi/getCourseDetails";
+
+    private static final RequestSpecification BASE_REQUEST_SPEC = new RequestSpecBuilder().setBaseUri(BASE_URI).build();
+    private static final ResponseSpecification STATUS_OK = new ResponseSpecBuilder().expectStatusCode(200).build();
+
+    private static final String CLIENT_ID = "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com";
+    private static final String CLIENT_SECRET = "erZOWM9g3UtwNRj340YYaK_W";
+    private static final String GRANT_TYPE = "client_credentials";
+    private static final String SCOPE = "scope";
+
     @Test
     public void testGetCourseDetailsWithOAuth(){
         //S1: get access token from oAuth API
-        RestAssured.baseURI = "https://rahulshettyacademy.com";
-        String responseStr = given().formParam("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
-                            .formParam("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
-                            .formParam("grant_type", "client_credentials")
-                            .formParam("scope", "scope")
-                            .when().post("/oauthapi/oauth2/resourceOwner/token")
-                            .then().assertThat().statusCode(200).extract().response().asString();
-        JsonPath js= new  JsonPath(responseStr);
+        String token = login();
+        assertNotNull(token, "Token is null");
+        assertFalse(token.isEmpty(), "Token is empty");
 
-        //S2: execute the getCourseDetails API using the access token and POJO to deserialize the JSON response
-        Response response = given().queryParam("access_token", js.getString("access_token"))
-                .when().get("/oauthapi/getCourseDetails");
+        //S2: execute the getCourseDetails API
+        getcourseDetails(token);
+    }
 
-        GetCourseDetailsResponse detailsObj = response.as(GetCourseDetailsResponse.class);
+    private String login(){
+        String responseStr = given()
+                .spec(BASE_REQUEST_SPEC)
+                .formParam("client_id", CLIENT_ID)
+                .formParam("client_secret", CLIENT_SECRET)
+                .formParam("grant_type", GRANT_TYPE)
+                .formParam("scope", SCOPE)
+                .when().post(AUTHORIZATION_ENDPOINT)
+                .then()
+                .spec(STATUS_OK)
+                .extract().response().asString();
+        return JsonPath.from(responseStr).getString("access_token");
+    }
+
+    private void getcourseDetails(String token){
+        //deserialize POJO the JSON response
+        GetCourseDetailsResponse detailsObj = given()
+                .spec(BASE_REQUEST_SPEC)
+                .queryParam("access_token", token)
+                .when().get(GET_COURSE_DETAILS_ENDPOINT)
+                .then().log().all().extract().response()
+                .as(GetCourseDetailsResponse.class);
+
         System.out.println("\n Using POJO class to deserialize the JSON response: \n Web Automation Courses:");
         detailsObj.getCourses().getWebAutomation().stream().filter(c -> c.getPrice() == 40)
                 .forEach(c -> {
                     System.out.println(" - Title: "  + c.getCourseTitle() + " - Price: "  + c.getPrice());
                 });
-
-        responseStr = response.asString();
-        System.out.println("\n getCourseDetails response string: " + responseStr);
-
-
     }
 }
